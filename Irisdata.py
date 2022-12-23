1bd5e7d2c3e463ebd7792e162fc63ccbf50698bf
Satwik Sangannagari <108742039+satwik12234@users.noreply.github.com>
2022-12-24 01:02:09 +0530

    Update Irisdata.py

--- Irisdata.py
+++ Irisdata.py
@@ -1,2 +1,3 @@
 url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv'
 col_name = ['sepal-lenght','sepal-width','petal-lenght','petal-width','class']
+dataset = pd.read_csv(url, names = col_name)
