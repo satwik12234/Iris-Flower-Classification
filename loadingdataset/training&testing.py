97655fd867208ce0f751eebae9a86e6d326ccca6
Satwik Sangannagari <108742039+satwik12234@users.noreply.github.com>
2023-09-23 12:12:01 +0530

    Delete loadingdataset/training&testing.py

--- loadingdataset/training&testing.py
+++
@@ -1,5 +0,0 @@
-training_size=int(len(closedf)*0.60)
-test_size=len(closedf)-training_size
-train_data,test_data=closedf[0:training_size,:],closedf[training_size:len(closedf),:1]
-print("train_data: ", train_data.shape)
-print("test_data: ", test_data.shape)
